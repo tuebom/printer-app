22f3aac3bf71554ec96e3a0a991a5e43600fccf8
Putu Wirya <tuebom.wirya@gmail.com>
2019-11-22 11:36:35 +0800

    first commit

---
+++ www/js/main.js
@@ -0,0 +1,59 @@
+var $$ = Dom7;
+
+var items   = [];
+
+var app = new Framework7({
+  root: '#app',
+  id:   'com.askitchen.printer-app',
+  name: 'Stock Info',
+
+  data: function () {
+    return {
+      // db: null,
+      // username: null,
+      // password: null,
+
+      endpoint: 'https://askitchen.com/api/v1/',
+
+    };
+  },
+  // App root methods
+  methods: {
+    
+    itemExists: function(kode) {
+      
+      var bFound = false;
+
+      for (var i=0; i < items.length; i++)
+        if (items[i].kdbar === kode) {
+          
+          bFound = true;
+          break;
+        }
+      return bFound;
+    },
+  },
+  routes: [
+    {
+      path: '/',
+      async: function (routeTo, routeFrom, resolve, reject) {
+        // Router instance
+        // var router = this;
+
+        // App instance
+        // var app = router.app;
+
+        // Resolve route to load page
+        resolve(
+          {
+            componentUrl: './pages/home.html',
+          }
+        );
+      },
+    }
+  ],
+});
+
+var mainView = app.views.create('.view-main', {
+  url: '/'
+});
